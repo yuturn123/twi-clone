a85e55da8b0d85593ee41900a50455ece1f3ad1d
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-01-22 17:20:36 +0900

    make app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import styles from './App.module.css';
+import {useSelector, useDispatch} from "react-redux";
+import {selectUser, login, logout} from "./features/userSlice";
+import { auth } from "./firebase";
+import Feed from './components/Feed';
+import Auth from './components/Auth';
 
-function App() {
+const App:React.FC = () =>  {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      }else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  },[dispatch]);
+
   return (
-    <div className="App"></div>
+  <>
+    {
+      user.uid ? (
+        <div className={styles.app}>
+          <Feed/>
+        </div>
+      ):(
+        <Auth/>
+      )
+    }
+  </>
   );
-}
+};
 
 export default App;
