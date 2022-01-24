c03167db4b757c1ecade32d45c14c08ba66862f8
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-01-24 22:36:26 +0900

    temporary commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,14 +5,14 @@ import "firebase/storage";
 
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: ".firebaseapp.com",
-    databaseURL: ".firebaseio.com",
-    projectId: "",
-    storageBucket: ".com",
-    messagingSenderId: "",
-    appId: ""
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
