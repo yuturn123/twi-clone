fa7e0e56ae495e33e444f7298b1466595ab9ba1b
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-01-25 21:45:34 +0900

    modify google-authentication make email-authentication

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -58,9 +58,21 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth:React.FC = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const signInEmail = async () =>{
+    await auth.signInWithEmailAndPassword(email, password);
+  };
+
+  const signUpEmail = async () => {
+    await auth.createUserWithEmailAndPassword(email,password);
+    console.log(password);
+  };
+
   const signInGoogle = async () => {
     await auth.signInWithPopup(provider).catch((err) => alert(err.message));
-    console.log(auth);
   };
 
   return (
@@ -73,7 +85,7 @@ const Auth:React.FC = () => {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            {isLogin ? "Login" : "Register"}
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
@@ -86,6 +98,10 @@ const Auth:React.FC = () => {
               name="email"
               autoComplete="email"
               autoFocus
+              value={email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+                setEmail(e.target.value);
+              }}
             />
             <TextField
               variant="outlined"
@@ -97,21 +113,57 @@ const Auth:React.FC = () => {
               type="password"
               id="password"
               autoComplete="current-password"
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setPassword(e.target.value);
+              }}
             />
 
             <Button
-
               fullWidth
               variant="contained"
               color="primary"
               className={classes.submit}
-
+              startIcon={<EmailIcon />}
+              onClick={
+                isLogin
+                  ? async () => {
+                      try {
+                        await signInEmail();
+                      } catch (err:any) {
+                        alert(err.message);
+                      }
+                    }
+                  : async () => {
+                      try {
+                        await signUpEmail();
+                      } catch (err:any) {
+                        alert(err.message);
+                      }
+                    }
+              }
             >
-              Sign In
+              {isLogin ? "Login" : "Register"}
             </Button>
+            <Grid container>
+              <Grid item xs>
+                <span
+                  className={styles.login_reset}
+                >
+                  Forgot password?
+                </span>
+              </Grid>
+              <Grid item xs>
+                <span
+                  className={styles.login_toggleMode}
+                  onClick={() => setIsLogin(!isLogin)}
+                >
+                  {isLogin ? "Create new account ?" : "Back to login"}
+                </span>
+              </Grid>
+            </Grid>
 
             <Button
-              type="submit"
               fullWidth
               variant="contained"
               color="primary"
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import {auth} from '../firebase';
+
 
 function Feed() {
-  return <div>feed</div>;
+  return <div>feed
+  <button onClick={() => auth.signOut()}>LogOut</button>
+
+  </div>;
 }
 
 export default Feed;
