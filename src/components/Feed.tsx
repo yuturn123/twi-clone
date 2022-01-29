5c0cb8de9cc4039d7542444e458a64c27a177403
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-01-29 21:45:21 +0900

    make twi-func

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {auth} from '../firebase';
+import TweetInput from './TweetInput';
 
 
 function Feed() {
-  return <div>feed
-  <button onClick={() => auth.signOut()}>LogOut</button>
-
-  </div>;
-}
+  return (
+  <div>
+    feed
+    <TweetInput />
+    <button onClick={() => auth.signOut()}>LogOut</button>
+  </div>
+  )
+};
 
 export default Feed;
