b5bf0034161ebe2d9778e952b5e5635b706a22cd
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-02-05 15:04:45 +0900

    final commit

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -7,7 +7,7 @@ import { selectUser } from "../features/userSlice";
 import { Avatar, Button, IconButton } from "@material-ui/core";
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 
-function TweetInput() {
+const TweetInput:React.FC = () => {
   const user = useSelector(selectUser);
   const [tweetImage, setTweetImage] = useState<File | null>(null);
   const [tweetMsg, setTweetMsg] = useState("");
